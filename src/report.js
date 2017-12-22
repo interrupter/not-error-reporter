f78c71c72cb8d5478cda15561a4933f301e36649
Aleksander Repin <alex.repin@mail.ru>
2017-12-22 18:37:12 +0400

    v0.0.1

---
+++ src/report.js
@@ -0,0 +1,9 @@
+class notErrorReporter{
+	constructor(){
+
+	}
+
+
+}
+
+module.exports = notErrorReporter;
---
+++ test/report.js
@@ -0,0 +1,12 @@
+const expect = require("chai").expect,
+	notErrorReporter = require('../index.js');
+
+
+describe("error-reporter", function() {
+	describe("create", function() {
+		it("empty options is passed", function() {
+			let report = new notErrorReporter();
+
+		});
+	});
+});
